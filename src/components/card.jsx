510021d7a7a3c3517a6183a6dca5f891acf37e8c
jjdavenport <jordanjamesdavenport@gmail.com>
2024-09-07 22:12:27 +0100

    add error loading waiting states

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,17 +5,35 @@ import { useMediaQuery } from "react-responsive";
 const Card = () => {
   const desktop = useMediaQuery({ minWidth: 768 });
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [wait, setWait] = useState(0);
+  const [waiting, setWaiting] = useState(false);
+
+  const fetchData = async () => {
+    const waitTime = (ms) => new Promise((i) => setTimeout(i, ms));
+    const time = new Date().getTime();
+    const fetchTime = time - wait;
+    if (fetchTime < 2000) {
+      setWaiting(true);
+      await waitTime(2000 - fetchTime);
+      setWaiting(false);
+    }
+    setLoading(true);
+    setError(false);
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const result = await response.json();
+      setData(result.slip);
+      setWait(new Date().getTime());
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://api.adviceslip.com/advice");
-        const result = await response.json();
-        setData(result.slip);
-        console.log("fetch", data);
-      } catch (error) {
-        console.log("error", error);
-      }
-    };
     fetchData();
   }, []);
 
@@ -23,12 +41,29 @@ const Card = () => {
     <>
       <main>
         <h1 className="flex gap-1">
-          {json.h1}
-          <span>{data.id}</span>
+          {waiting ? (
+            "Waiting for Data"
+          ) : loading ? (
+            "Loading..."
+          ) : error ? (
+            "Error"
+          ) : (
+            <>
+              {json.h1} <span>{data.id}</span>
+            </>
+          )}
         </h1>
-        <p>{data.advice}</p>
+        <p>
+          {waiting
+            ? "Waiting for Data"
+            : loading
+              ? "Loading..."
+              : error
+                ? "Error"
+                : data.advice}
+        </p>
         <img src={desktop ? json.desktop : json.mobile} alt="divider" />
-        <button>
+        <button onClick={fetchData}>
           <img src={json.buttonIcon} />
         </button>
       </main>
